6e89ead58695bd95b95425d81a803b59eb6d3b3a
Камал Эфендиев <kamalefendiev@MacBook-Air-Kamal.local>
2025-12-08 03:23:22 +0300

    feat: add vitest tests and github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig as defineVitestConfig } from 'vitest/config' // ← импортируем из vitest
 
 export default defineConfig({
   plugins: [react()],
   base: '/lab1-react-vite-gh-pages',
-  test: { // ← теперь TypeScript знает, что test — это валидное поле
+  test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
